3acab5da00a11a0573c642e19700446640bc966f
Fatih Erbil <Fatiheee@icloud.com>
2023-01-05 20:02:17 +0300

    tamamlandi

---
+++ script.js
@@ -0,0 +1,48 @@
+const wrapper    =   document.querySelector(".wrapper");
+const selectBtn  =   wrapper.querySelector(".select-btn");
+const searchInp  =   wrapper.querySelector("input");
+const options    =   wrapper.querySelector(".options");
+
+let citys    =   ['Adana', 'Adıyaman', 'Afyon', 'Ağrı', 'Amasya', 'Ankara', 'Antalya', 'Artvin',
+'Aydın', 'Balıkesir', 'Bilecik', 'Bingöl', 'Bitlis', 'Bolu', 'Burdur', 'Bursa', 'Çanakkale',
+'Çankırı', 'Çorum', 'Denizli', 'Diyarbakır', 'Edirne', 'Elazığ', 'Erzincan', 'Erzurum', 'Eskişehir',
+'Gaziantep', 'Giresun', 'Gümüşhane', 'Hakkari', 'Hatay', 'Isparta', 'Mersin', 'İstanbul', 'İzmir', 
+'Kars', 'Kastamonu', 'Kayseri', 'Kırklareli', 'Kırşehir', 'Kocaeli', 'Konya', 'Kütahya', 'Malatya', 
+'Manisa', 'Kahramanmaraş', 'Mardin', 'Muğla', 'Muş', 'Nevşehir', 'Niğde', 'Ordu', 'Rize', 'Sakarya',
+'Samsun', 'Siirt', 'Sinop', 'Sivas', 'Tekirdağ', 'Tokat', 'Trabzon', 'Tunceli', 'Şanlıurfa', 'Uşak',
+'Van', 'Yozgat', 'Zonguldak', 'Aksaray', 'Bayburt', 'Karaman', 'Kırıkkale', 'Batman', 'Şırnak',
+'Bartın', 'Ardahan', 'Iğdır', 'Yalova', 'Karabük', 'Kilis', 'Osmaniye', 'Düzce']
+
+function addcity(selectedcity){
+    options.innerHTML = "";
+    options.innerHTML = "";
+    citys.forEach(city => {
+        let isSelected = city == selectedcity ? "selected" : "";
+        let li = `<li onclick="updateName(this)" class="${isSelected}">${city}</li>`;
+        options.insertAdjacentHTML("beforeend", li);
+    });
+}
+addcity();
+
+function updateName(selectedLi){
+    searchInp.value = "";
+    addcity(selectedLi.innerText);
+    wrapper.classList.remove("active");
+    selectBtn.firstElementChild.innerText = selectedLi.innerText;
+}
+
+searchInp.addEventListener("keyup", () => {
+    let arr = [];
+    let searchedVal = searchInp.value.toLowerCase();
+    arr  =  citys.filter(data => {
+        return data.toLowerCase().startsWith(searchedVal);
+    }).map(data => {
+        let isSelected = data == selectBtn.firstElementChild.innerText ? "selected" : "";
+        return `<li onclick="updateName(this)" class="${isSelected}">${data}</li>`;
+    }).join("");
+    options.innerHTML = arr ? arr : `<p style="margin-top: 10px;">Oops! city not found</p>`;
+});
+
+selectBtn.addEventListener("click", () => {
+    wrapper.classList.toggle("active");
+});
